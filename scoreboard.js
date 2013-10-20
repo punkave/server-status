58d25a8edaaaf6075173fa82a3522562a9260d4a
Tom Boutell <tom@punkave.com>
2013-10-20 16:03:35 -0400

    Deltas, snapshot database

--- scoreboard.js
+++ scoreboard.js
@@ -1,6 +1,7 @@
 var config = require('./config.js');
 var express = require('express');
 var nunjucks = require('nunjucks');
+var fs = require('fs');
 var _ = require('lodash');
 
 var app = express();
@@ -15,12 +16,30 @@ var apikey = config.apikey;
 var interval = config.interval;
 var scoreboard = {};
 
+var snapshotFile = __dirname + '/snapshot.json';
+
+if (fs.existsSync(snapshotFile)) {
+  scoreboard = JSON.parse(fs.readFileSync(snapshotFile), function(k, v) {
+    // Reviver to deal with date strings and turn them back into dates. JSON
+    // makes me pine for PHP's serialize... OK just a little
+    if (k === 'when') {
+      return new Date(v);
+    }
+    return v;
+  });
+}
+
+// API for submitting a report from a server
 app.post('/report', function(req, res) {
   if (req.body.apikey !== apikey) {
     res.statusCode = 404;
     return res.send('notfound');
   }
   _.each(req.body.sites, function(site) {
+    site.previous = scoreboard[site.name];
+    if (site.previous) {
+      delete site.previous.previous;
+    }
     site.when = new Date();
     site.disk = req.body.disk;
     site.cpu = req.body.cpu;
@@ -29,6 +48,16 @@ app.post('/report', function(req, res) {
   return res.send('ok');
 });
 
+// API for data access
+app.get('/data', function(req, res) {
+  if (req.query.apikey !== apikey) {
+    res.statusCode = 404;
+    return res.send('notfound');
+  }
+  return res.send(JSON.stringify(scoreboard));
+});
+
+// UI for end users & dedicated scoreboard monitors
 app.get('/', function(req, res) {
   if (req.query.apikey === apikey) {
     res.cookie('apikey', req.query.apikey);
@@ -75,6 +104,13 @@ app.get('/logout', function(req, res) {
   return res.send('logged out');
 });
 
+// Save a JSON snapshot of what we know every 30 seconds
+// so we can easily restart
+setInterval(snapshot, 30000);
+
 console.log('Listening on port ' + port);
 app.listen(port);
 
+function snapshot() {
+  fs.writeFileSync(__dirname + '/snapshot.json', JSON.stringify(scoreboard));
+}
